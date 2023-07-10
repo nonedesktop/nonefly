dc79df9a19bbc1dfd2e89811d97ce2af0e96d41c
worldmozara <worldmozara@163.com>
2023-07-11 00:29:04 +0800

    feat: add a simple api

--- src/noneflyd/__init__.py
+++ src/noneflyd/__init__.py
@@ -9,6 +9,12 @@ def status():
     return {"message": "active"}
 
 
+@app.get("/nonefly/simple/file/list")
+@app.get("/nonefly/simple/instances/{instance}/file/list")
+def file_list(instance: str, dir: str, token: str):
+    return {"instance": instance, "directory": dir, "token": token}
+
+
 @app.websocket("/nonefly/ws")
 def wsconn():
     ...
